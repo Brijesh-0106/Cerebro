5c85a4650e8ab4cb1c0c28bfa57a03d00a2e9805
phenomenal <brijeshshah654@gmail.com>
2026-02-15 21:46:46 +0530

    any added in error and result type in storage

--- secondBrainBackend/src/storage.ts
+++ secondBrainBackend/src/storage.ts
@@ -11,7 +11,7 @@ export const uploadImage = (fileBuffer: Buffer): Promise<string> => {
     return new Promise((resolve, reject) => {
         const stream = cloudinary.uploader.upload_stream(
             { folder: "cerebro" },
-            (error, result) => {
+            (error: any, result: any) => {
                 if (error) return reject(error);
                 resolve(result!.secure_url);
             }
